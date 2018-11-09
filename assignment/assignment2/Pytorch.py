fd6f64121cc3138316d10145e6b405d14dc39c5a
duanyiting2018 <1036179833@qq.com>
2018-11-09 22:31:20 +0800

    pytorch

---
+++ assignment/assignment2/Pytorch.py
@@ -0,0 +1,354 @@
+# -*- coding: utf-8 -*-
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+
+import numpy as np
+
+
+NUM_TRAIN = 49000
+
+# The torchvision.transforms package provides tools for preprocessing data
+# and for performing data augmentation; here we set up a transform to
+# preprocess the data by subtracting the mean RGB value and dividing by the
+# standard deviation of each RGB value; we've hardcoded the mean and std.
+transform = T.Compose([
+                T.ToTensor(),
+                T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+            ])
+
+# We set up a Dataset object for each split (train / val / test); Datasets load
+# training examples one at a time, so we wrap each Dataset in a DataLoader which
+# iterates through the Dataset and forms minibatches. We divide the CIFAR-10
+# training set into train and val sets by passing a Sampler object to the
+# DataLoader telling how it should sample from the underlying Dataset.
+cifar10_train = dset.CIFAR10('./cs231n/datasets', train=True, download=True,
+                             transform=transform)
+loader_train = DataLoader(cifar10_train, batch_size=64, 
+                          sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN)))
+
+cifar10_val = dset.CIFAR10('./cs231n/datasets', train=True, download=True,
+                           transform=transform)
+loader_val = DataLoader(cifar10_val, batch_size=64, 
+                        sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN, 50000)))
+
+cifar10_test = dset.CIFAR10('./cs231n/datasets', train=False, download=True, 
+                            transform=transform)
+loader_test = DataLoader(cifar10_test, batch_size=64)
+
+
+
+USE_GPU = True
+
+dtype = torch.float32 # we will be using float throughout this tutorial
+
+if USE_GPU and torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+# Constant to control how frequently we print train loss
+print_every = 100
+
+print('using device:', device)
+
+
+def flatten(x):
+    N = x.shape[0] # read in N, C, H, W
+    return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+
+def test_flatten():
+    x = torch.arange(12).view(2, 1, 3, 2)
+    print('Before flattening: ', x)
+    print('After flattening: ', flatten(x))
+
+test_flatten()
+
+#Barebones PyTorch: Two-Layer Network
+import torch.nn.functional as F  # useful stateless functions
+
+def two_layer_fc(x, params):
+    """
+    A fully-connected neural networks; the architecture is:
+    NN is fully connected -> ReLU -> fully connected layer.
+    Note that this function only defines the forward pass; 
+    PyTorch will take care of the backward pass for us.
+    
+    The input to the network will be a minibatch of data, of shape
+    (N, d1, ..., dM) where d1 * ... * dM = D. The hidden layer will have H units,
+    and the output layer will produce scores for C classes.
+    
+    Inputs:
+    - x: A PyTorch Tensor of shape (N, d1, ..., dM) giving a minibatch of
+      input data.
+    - params: A list [w1, w2] of PyTorch Tensors giving weights for the network;
+      w1 has shape (D, H) and w2 has shape (H, C).
+    
+    Returns:
+    - scores: A PyTorch Tensor of shape (N, C) giving classification scores for
+      the input data x.
+    """
+    # first we flatten the image
+    x = flatten(x)  # shape: [batch_size, C x H x W]
+    
+    w1, w2 = params
+    
+    # Forward pass: compute predicted y using operations on Tensors. Since w1 and
+    # w2 have requires_grad=True, operations involving these Tensors will cause
+    # PyTorch to build a computational graph, allowing automatic computation of
+    # gradients. Since we are no longer implementing the backward pass by hand we
+    # don't need to keep references to intermediate values.
+    # you can also use `.clamp(min=0)`, equivalent to F.relu()
+    x = F.relu(x.mm(w1))
+    x = x.mm(w2)
+    return x
+    
+
+def two_layer_fc_test():
+    hidden_layer_size = 42
+    x = torch.zeros((64, 50), dtype=dtype)  # minibatch size 64, feature dimension 50
+    w1 = torch.zeros((50, hidden_layer_size), dtype=dtype)
+    w2 = torch.zeros((hidden_layer_size, 10), dtype=dtype)
+    scores = two_layer_fc(x, [w1, w2])
+    print(scores.size())  # you should see [64, 10]
+
+two_layer_fc_test()
+
+#Barebones PyTorch: Three-Layer ConvNet
+
+
+def three_layer_convnet(x, params):
+    """
+    Performs the forward pass of a three-layer convolutional network with the
+    architecture defined above.
+
+    Inputs:
+    - x: A PyTorch Tensor of shape (N, 3, H, W) giving a minibatch of images
+    - params: A list of PyTorch Tensors giving the weights and biases for the
+      network; should contain the following:
+      - conv_w1: PyTorch Tensor of shape (channel_1, 3, KH1, KW1) giving weights
+        for the first convolutional layer
+      - conv_b1: PyTorch Tensor of shape (channel_1,) giving biases for the first
+        convolutional layer
+      - conv_w2: PyTorch Tensor of shape (channel_2, channel_1, KH2, KW2) giving
+        weights for the second convolutional layer
+      - conv_b2: PyTorch Tensor of shape (channel_2,) giving biases for the second
+        convolutional layer
+      - fc_w: PyTorch Tensor giving weights for the fully-connected layer. Can you
+        figure out what the shape should be?
+      - fc_b: PyTorch Tensor giving biases for the fully-connected layer. Can you
+        figure out what the shape should be?
+    
+    Returns:
+    - scores: PyTorch Tensor of shape (N, C) giving classification scores for x
+    """
+    conv_w1, conv_b1, conv_w2, conv_b2, fc_w, fc_b = params
+    scores = None
+    ################################################################################
+    # TODO: Implement the forward pass for the three-layer ConvNet.                #
+    ################################################################################
+ 
+    #x input : minibatch×in_channels×iH×iW   (N, 3, H, W) 
+    # weight ：out_channels×in_channels/groups×kH×kW    (channel_1, 3, KH1, KW1)
+    # bias：out_channels (channel_1,)
+    # torch.nn.Conv2d ：无需自定义w，b ；_ConvNd父类实现w，b
+    # F.conv2d 自定义w，b
+    
+    out = F.conv2d(x, conv_w1, bias=conv_b1, stride=1, padding=2, dilation=1, groups=1)  
+    out = F.relu(out)
+    #conv_w2  (channel_2, channel_1, KH2, KW2) 
+    out = F.conv2d(out, conv_w2, bias=conv_b2, stride=1, padding=1, dilation=1, groups=1)  
+    out = F.relu(out)    
+    # out ：(N, C_{out}, H_{out}, W_{out})
+    out = flatten(out) # (N, -1)
+    scores =  out.mm(fc_w) + fc_b
+    
+    
+    ################################################################################
+    #                                 END OF YOUR CODE                             #
+    ################################################################################
+    return scores
+
+
+def three_layer_convnet_test():
+    x = torch.zeros((64, 3, 32, 32), dtype=dtype)  # minibatch size 64, image size [3, 32, 32]
+
+    conv_w1 = torch.zeros((6, 3, 5, 5), dtype=dtype)  # [out_channel, in_channel, kernel_H, kernel_W]
+    conv_b1 = torch.zeros((6,))  # out_channel
+    # out:(N, C_{out}, H_{out}, W_{out})
+    # (W-F + 2P )/S +1  :  (32 - 5 +2 *2 ) / 1 + 1 = 32 
+    # out :(64,6,32,32)
+    
+    conv_w2 = torch.zeros((9, 6, 3, 3), dtype=dtype)  # [out_channel, in_channel, kernel_H, kernel_W]
+    conv_b2 = torch.zeros((9,))  # out_channel
+    # out:(N, C_{out}, H_{out}, W_{out})
+    # (W-F + 2P )/S +1  :  (32 - 3 +2 *1 ) / 1 + 1 = 32 
+    # out :(64,9,32,32)    
+    
+
+    # out flatten:(64,9,32,32) ---->  (64,9*32*32)
+    # you must calculate the shape of the tensor after two conv layers, before the fully-connected layer
+    fc_w = torch.zeros((9 * 32 * 32, 10))
+    fc_b = torch.zeros(10)
+
+    scores = three_layer_convnet(x, [conv_w1, conv_b1, conv_w2, conv_b2, fc_w, fc_b])
+    print(scores.size())  # you should see [64, 10]
+three_layer_convnet_test()
+
+
+
+#Barebones PyTorch: Initialization
+def random_weight(shape):
+    """
+    Create random Tensors for weights; setting requires_grad=True means that we
+    want to compute gradients for these Tensors during the backward pass.
+    We use Kaiming normalization: sqrt(2 / fan_in)
+    """
+    if len(shape) == 2:  # FC weight
+        fan_in = shape[0]
+    else:
+        fan_in = np.prod(shape[1:]) # conv weight [out_channel, in_channel, kH, kW]
+    # randn is standard normal distribution generator. 
+    w = torch.randn(shape, device=device, dtype=dtype) * np.sqrt(2. / fan_in)
+    w.requires_grad = True
+    return w
+
+def zero_weight(shape):
+    return torch.zeros(shape, device=device, dtype=dtype, requires_grad=True)
+
+# create a weight of shape [3 x 5]
+# you should see the type `torch.cuda.FloatTensor` if you use GPU. 
+# Otherwise it should be `torch.Tensor`
+random_weight((3, 5))
+
+
+#Barebones PyTorch: Check Accuracy
+def check_accuracy_part2(loader, model_fn, params):
+    """
+    Check the accuracy of a classification model.
+    
+    Inputs:
+    - loader: A DataLoader for the data split we want to check
+    - model_fn: A function that performs the forward pass of the model,
+      with the signature scores = model_fn(x, params)
+    - params: List of PyTorch Tensors giving parameters of the model
+    
+    Returns: Nothing, but prints the accuracy of the model
+    """
+    split = 'val' if loader.dataset.train else 'test'
+    print('Checking accuracy on the %s set' % split)
+    num_correct, num_samples = 0, 0
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.int64)
+            scores = model_fn(x, params)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f%%)' % (num_correct, num_samples, 100 * acc))
+
+
+
+def train_part2(model_fn, params, learning_rate):
+    """
+    Train a model on CIFAR-10.
+    
+    Inputs:
+    - model_fn: A Python function that performs the forward pass of the model.
+      It should have the signature scores = model_fn(x, params) where x is a
+      PyTorch Tensor of image data, params is a list of PyTorch Tensors giving
+      model weights, and scores is a PyTorch Tensor of shape (N, C) giving
+      scores for the elements in x.
+    - params: List of PyTorch Tensors giving weights for the model
+    - learning_rate: Python scalar giving the learning rate to use for SGD
+    
+    Returns: Nothing
+    """
+    for t, (x, y) in enumerate(loader_train):
+        # Move the data to the proper device (GPU or CPU)
+        x = x.to(device=device, dtype=dtype)
+        y = y.to(device=device, dtype=torch.long)
+
+        # Forward pass: compute scores and loss
+        scores = model_fn(x, params)
+        loss = F.cross_entropy(scores, y)
+
+        # Backward pass: PyTorch figures out which Tensors in the computational
+        # graph has requires_grad=True and uses backpropagation to compute the
+        # gradient of the loss with respect to these Tensors, and stores the
+        # gradients in the .grad attribute of each Tensor.
+        loss.backward()
+
+        # Update parameters. We don't want to backpropagate through the
+        # parameter updates, so we scope the updates under a torch.no_grad()
+        # context manager to prevent a computational graph from being built.
+        with torch.no_grad():
+            for w in params:
+                w -= learning_rate * w.grad
+
+                # Manually zero the gradients after running the backward pass
+                w.grad.zero_()
+
+        if t % print_every == 0:
+            print('Iteration %d, loss = %.4f' % (t, loss.item()))
+            check_accuracy_part2(loader_val, model_fn, params)
+            print()
+            
+hidden_layer_size = 4000
+learning_rate = 1e-2
+
+w1 = random_weight((3 * 32 * 32, hidden_layer_size))
+w2 = random_weight((hidden_layer_size, 10))
+
+train_part2(two_layer_fc, [w1, w2], learning_rate)
+
+
+
+
+learning_rate = 3e-3
+
+channel_1 = 32
+channel_2 = 16
+
+################################################################################
+# TODO: Initialize the parameters of a three-layer ConvNet.                    #
+#for t, (x, y) in enumerate(loader_train):
+#    print(x.shape)
+#[64, 3, 32, 32]
+################################################################################
+conv_w1 = random_weight((channel_1, 3, 5, 5))
+conv_b1 = zero_weight((channel_1))
+conv_w2 = random_weight((channel_2, channel_1, 3, 3))
+conv_b2 = zero_weight(channel_2)
+fc_w = random_weight((channel_2 * 32 * 32, 10))
+fc_b = zero_weight(10)
+################################################################################
+#                                 END OF YOUR CODE                             #
+################################################################################
+
+params = [conv_w1, conv_b1, conv_w2, conv_b2, fc_w, fc_b]
+train_part2(three_layer_convnet, params, learning_rate)
+
+
+#Part III. PyTorch Module API
+#Module API: Two-Layer Network
+
+
+
+
+
+
+
+
+
+
+
